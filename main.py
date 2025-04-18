0dfae9f9dc47772c3bedc6cb0ec9afa36eafb5f4
Alf0nce <alfoncekngetich@gmail.com>
2025-04-18 14:00:23 +0300

    Group  Assignment Submition

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+from pet import Pet  # Import the Pet class from pet_class.py
+
+# Create a pet
+my_pet = Pet("Fluffy")
+
+# Interact with the pet
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.get_status()
+
+# Teach tricks
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.show_tricks()
